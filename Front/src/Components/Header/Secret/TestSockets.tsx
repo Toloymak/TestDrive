1ad46c3cf529417ea29cb5864dfb58f234460bef
Mello <mello.shlyapa@gmail.com>
2019-11-12 19:20:24 +0500

    секретное окно тестов сокетов

---
+++ Front/src/Components/Header/Secret/TestSockets.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import {testSocket} from './utils';
+import {useEffect, useState} from 'react';
+
+interface Props {
+    visibleListTestSocket: boolean;
+}
+
+export const TestSocket: React.FC<Props> = ({visibleListTestSocket}) => {
+
+    const [link, setLink] = useState('');
+    const [block, setBlock] = useState('');
+    const [front, setFront] = useState('');
+
+    useEffect(() => {
+        const allSocket = testSocket();
+        allSocket.link.getData(setLink)
+        allSocket.block.getData(setBlock)
+        allSocket.front.getData(setFront)
+    }, [])
+
+    return <div className="testSocket">
+      <div className="testSocket_links">
+          <h2>_links</h2>
+          <span>{JSON.stringify(link)}</span>
+      </div>
+      <div className="testSocket_blocks">
+          <h2>_blocks</h2>
+          <span>{JSON.stringify(block)}</span>
+      </div>
+      <div className="testSocket_front">
+          <h2>_front</h2>
+          <span>{JSON.stringify(front)}</span>
+      </div>
+    </div>
+}
---
+++ Front/src/Components/Header/Secret/utils.ts
@@ -0,0 +1,10 @@
+import {SocketHubs} from 'src/enums';
+import {SocketBase} from 'src/utils/SocketBase';
+
+export const testSocket = () => {
+    return {
+        link: new SocketBase(SocketHubs.links),
+        block: new SocketBase(SocketHubs.blocks),
+        front: new SocketBase(SocketHubs.frontLinks),
+    }
+}
