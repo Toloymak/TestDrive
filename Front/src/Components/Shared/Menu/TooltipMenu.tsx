b943d03030923f1631b313931e57835c5c4c9de9
Mello <mello.shlyapa@gmail.com>
2019-11-29 14:49:26 +0500

    закрывашка меню

--- Front/src/Components/Shared/Menu/TooltipMenu.tsx
+++ Front/src/Components/Shared/Menu/TooltipMenu.tsx
@@ -9,7 +9,7 @@ interface Props {
 export const TooltipMenu: React.FC<Props> = ({ close, children }) => {
     useEffect(() => {
         document.addEventListener('mouseup', event => {
-            const element = document.querySelector('.menuItem');
+            const element = document.querySelector(`.${style.menuBlock}`);
             if (element && !element.contains(event.target as Element)) {
                 close();
             }
